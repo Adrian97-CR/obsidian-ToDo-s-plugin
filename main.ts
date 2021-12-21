901a80f32fe6481fb7942aa8f7fb15ebb3c5237e
Adrian97-CR <37858320+Adrian97-CR@users.noreply.github.com>
2021-12-21 00:24:59 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,162 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { read } from 'fs';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, ListedFiles } from 'obsidian';
+import { arrayBuffer } from 'stream/consumers';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface ToDosSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+interface Note {
+	name:string;
+	deadline:Date;
+	status:string;
+	objective:string[],
+	insert:boolean
+}
+const DEFAULT_SETTINGS: ToDosSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class toDosPlugin extends Plugin {
+	settings: ToDosSettings;
+	getNewTask(reader:any, note:Note, edit?:any){
+		let auxTask:string[] = []
+		reader = reader.split('\n');
+		note.deadline = new Date(reader[0].split('-'))
+		let i = 2;
+		for (; i < reader.length; i++) {
+			if(reader[i].substring(0,1)!=='#') {
+				auxTask.push(reader[i])
+			}
+			else break;
+		}
+		i++;
+		note.objective = auxTask;
+		note.status = reader[i];
+		return
+	}
+	sortFunction(a:Note,b:Note){
+		var dateA = a.deadline.getTime();
+		var dateB = b.deadline.getTime();
+		return dateA > dateB ? 1 : -1;  
+	};
+	checkCoincidence(notes:Note[],name:string, i:number):boolean
+	{
+		if (notes[i]!==undefined) {
+			console.log(notes[i].name,name,1);
+			return notes[i].name===name || this.checkCoincidence(notes,name,i+1);
+		}
+		return false; // acomodoar returns recursivos para filtrar las que se eliminan
+	}
+	async toDosUpdate(notes:Note[]) {
+		let toDos = (await this.app.vault.adapter.read("ToDo's.md")).split('\n');
+		toDos.forEach((line:string, index)=>{toDos[index] = line+'\n'})
+		let newToDos = toDos[0]+toDos[1]+toDos[2];
+		let auxNotes:Note[] = [];
+		let cont = 0;
+		let endBand = false;
+		for (let i = 3; i < toDos.length; i++) {   //  esta entrando aqui sin insertar las notas si son las ultimas tareas
+			//ademas está tomando -1 dia en las fechas en toDos no se porque
+			if (toDos[i].substring(0,1) === '#') {   //  entra a Completadas
+				endBand = true;
+				notes.map((nte:Note)=>{newToDos+=nte.name.substring(0,nte.name.length-3)+'|'+nte.objective[0]+'|'+nte.deadline.getFullYear()+'-'+(nte.deadline.getMonth()+1)+'-'+nte.deadline.getDate()+'|'+'|'+'\n';});
+				newToDos = newToDos + toDos[i]+toDos[i+1]+toDos[i+2]
+				i += 2;
+
+				////////////////           segun yo esto elimina las notas recuperadas/ comprobar eliminar completadas, 
+				toDos = toDos.slice(i).filter((line:string)=>{
+					let spl = line[i].split('|');
+					return !this.checkCoincidence(auxNotes,spl[1],0)
+				})
+				toDos.slice(1).map((line)=>{newToDos+=line});
+				newToDos.substring(0,toDos.length-3)
+				break;
+				
+			}
+			cont = 0;
+			let line = toDos[i].split('|');
+			while(cont<notes.length){ //  agrega las notas con prioridad de tiempo
+				if (notes[cont].deadline.getTime() < (new Date(line[2])).getTime()) {
+					newToDos += notes[cont].name.substring(0,notes[cont].name.length-3)+'|'+notes[cont].objective[0]+'|'+notes[cont].deadline.getFullYear()+'-'+(notes[cont].deadline.getMonth()+1)+'-'+notes[cont].deadline.getDate()+'|'+'|'+'\n';
+					if (notes[cont].name.substring(0,2) === '10') {
+						auxNotes.concat(notes.splice(cont,1));					
+					}
+					notes.splice(cont,1);
+					cont--;
+					if (notes.length==0) {
+						while(toDos[i].substring(0,1) !== '#'){
+							newToDos += toDos[i]
+							i++;
+						}
+						i--;
+						break;
+					}
+				}
+				else {
+					newToDos += toDos[i];
+					break;
+				}
+				cont++;
+			}
+			
+			
+		}// hacer test de todo
+		
+		console.log(newToDos);
+	}
+	// while(cont<notes.length){
+	// 	let line = toDos[i].split('|');
+	// 	if (line[0].substring(3, auxNote.name.length-1) === auxNote.name.substring(2)) {  //  que sea 
+	// 		newToDos = newToDos+line[0]+'|'+auxNote.deadline.getDate()+'|\n';
+	// 		break;
+	// 	}
+	// 	else if (auxNote.deadline.getTime() < (new Date(line[2])).getTime()) {
+	// 		break;
+	// 	}
+	// 	else newToDos += toDos[i];
+	// }
+
+	handleScan(){
+		this.app.vault.adapter.list("ToDo's").then(async(reader:ListedFiles)=>{
+			let listToDos:Note[] = [];
+			let listToUpdate:Note[] = [];
+			reader.files.forEach((fileName:string) => {
+				fileName = fileName.split('/')[1];
+				// Busca la tarea nueva sin insertar para insertar en todo's
+				if(fileName.substring(0,2) === '00'){
+					listToDos.push({name:fileName,status:"",deadline:new Date,objective:[""],insert:true});
+				}
+				// busca los archivos que tengan la tarea completada con objetivo nuevo para insertar en todo's
+				else if(fileName.substring(0,2) === '10'){
+					listToDos.push({name:fileName,status:"",deadline:new Date,objective:[""],insert:true});
+				}
+				// Busca notas con cambios para actualizar avance(listo)
+				else if(fileName.substring(0,2) === '01'){    //////////////           pendiente
+					listToUpdate.push({name:fileName,status:"",deadline:new Date,objective:[""],insert:true});
+				}
+			});
+			// extrae la info de las notas para actualizar toDos
+			if (listToDos.length!=0) {
+				for (let i = 0; i < listToDos.length; i++) {
+					let reader = await this.app.vault.adapter.read("ToDo's/"+listToDos[i].name);
+					this.getNewTask(reader, listToDos[i]);
+				}
+				listToDos.sort(this.sortFunction);
+				this.toDosUpdate(listToDos);
+
+			}
+			
+		})
+	}
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			this.handleScan();
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
@@ -108,9 +245,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: toDosPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: toDosPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
